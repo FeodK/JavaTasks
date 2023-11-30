f4a17a93c80b91644a9f7a44a3b1af2a94edf9d7
Feodor <mr.fedor.kiselev@gmail.com>
2023-11-30 18:33:01 +0300

    Task4

---
+++ Task4/Task4.java
@@ -0,0 +1,23 @@
+/*Дан массив двоичных чисел, например [1,1,0,1,1,1], вывести максимальное количество подряд идущих 1.*/
+
+package Task4;
+
+public class Task4 {
+    public static void main(String[] args) {
+        int[] array = new int[] {1,1,0,1,1,1};
+        int count = 0;
+        int max = 0;
+        for (int i : array){
+            if(i == 1) {
+                count++;
+            }
+            else{
+                if(max < count) max = count;
+                count = 0;
+            }
+
+        }
+        if(max < count) max = count;
+        System.out.println(max);
+    }
+}
