010ec37a0cfbf81ac8c78be2528792d310be619a
Feodor <mr.fedor.kiselev@gmail.com>
2023-11-30 17:42:59 +0300

    Task1

---
+++ Task1/Task1.java
@@ -0,0 +1,13 @@
+/*Настроить проект, вывести в консоль “Hello world!”.
+Вывести в консоль системные дату и время.*/
+
+package Task1;
+
+import java.time.LocalDateTime;
+
+public class Task1 {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println(LocalDateTime.now());
+    }
+}
